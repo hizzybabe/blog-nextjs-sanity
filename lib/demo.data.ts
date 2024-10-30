2b491d710786a7a5e2ee131053e3c361ced83099
hizzybabe <napipat_s@hotmail.com>
2024-10-30 14:12:17 +0700

    Update demo.data.ts

--- lib/demo.data.ts
+++ lib/demo.data.ts
@@ -1,6 +1,6 @@
 // All the demo data that used as fallbacks when there's nothing in the dataset yet
 
-export const title = TechFocus.Pro'
+export const title = 'TechFocus.Pro'
 
 export const description = [
   {
